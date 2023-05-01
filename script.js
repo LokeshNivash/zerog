9ad45d1d3feefca034700bea825000f20f0cb9f9
LokeshNivash <nivashnisa@gmail.com>
2023-05-02 01:32:09 +0530

    --

--- script.js
+++ script.js
@@ -7,25 +7,78 @@
 
 // })
 
-function classToggle() {
-    const navs = document.querySelectorAll('.menu')
+// function classToggle() {
+//     const navs = document.querySelectorAll('.menu')
     
-    navs.forEach(nav => nav.classList.toggle('active'));
-  }
+//     navs.forEach(nav => nav.classList.toggle('active'));
+//   }
   
-  document.querySelector('.menu-btn')
-    .addEventListener('click', classToggle);
+//   document.querySelector('.menu-btn')
+//     .addEventListener('click', classToggle);
 
 
 
+ 
 
+// const load = document.querySelector(".loader");
+// window.addEventListener("load", function () {
+//   load.style.display = "none";
+
+//   $( '.menu li a' ).on("click", function(){
+//     $('#menu').remove;
+//   });
+// });
+
+
+// ==============cg=================
+
+
+// function classToggle() {
+//   const navs = document.querySelectorAll('.menu')
+//   navs.forEach(nav => nav.classList.toggle('active'));
+// }
+
+// function closeMenu() {
+//   const navs = document.querySelectorAll('.menu')
+//   navs.forEach(nav => nav.classList.remove('active'));
+// }
+
+// document.querySelector('.menu-btn')
+//   .addEventListener('click', classToggle);
+
+// const menuItems = document.querySelectorAll('.menu-item');
+// menuItems.forEach(item => item.addEventListener('click', closeMenu));
+
+// const load = document.querySelector(".loader");
+// window.addEventListener("load", function () {
+//   load.style.display = "none";
+// });
+
+
+function classToggle() {
+  const navs = document.querySelectorAll('.menu')
+  navs.forEach(nav => nav.classList.toggle('active'));
+}
+
+function closeMenu() {
+  const navs = document.querySelectorAll('.menu')
+  navs.forEach(nav => nav.classList.remove('active'));
+}
+
+document.querySelector('.menu-btn')
+  .addEventListener('click', classToggle);
+
+const menuItems = document.querySelectorAll('.menu');
+menuItems.forEach(item => item.addEventListener('click', closeMenu));
 
 const load = document.querySelector(".loader");
 window.addEventListener("load", function () {
   load.style.display = "none";
 });
 
-const scriptURL =
+
+
+const scriptURL   =
     "https://script.google.com/macros/s/AKfycbz_-9vxy5uY_QIpJi0-2wLjViRDdn-4AD61nVJLGlvLgp2lsAe0qgeIi0mtmUsthqki/exec";
   const form = document.forms["submit-to-google-sheet"];
   const msg = document.getElementById("msg");
@@ -46,7 +99,26 @@ const scriptURL =
 
 //close menu bar
 
-$('.menu ul li a').on("click", function (e) {
-  burger.click().removeClass("active");
-});
+// $('.menu li a').onclick("click", function (e) {
+//   navs.click().removeClass("active");
+// });
 
+// function closeMenu(){
+//   // add class to the menu to make it show
+//   menu.classList.remove('active');
+//   // add event listener to the html element
+//   html.removeEventListener('click', closeMenuOnBodyClick);
+// }
+// function closeMenuOnBodyClick(event){
+//   closeMenu();
+// }
+// function closeMenuOnBodyClick(event){
+//   // get the event path
+//   const path = event.composedPath();
+//   // check if it has the menu element
+//   if (path.some(elem => elem.id === 'myMenuId')) {
+//     // terminate this function if it does
+//     return;
+//   }
+//   closeMenu();
+// }
